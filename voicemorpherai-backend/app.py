c3cafbf0af1558e5a62ca6e3ef321f438653b4a4
Ashwin Bijoy <ashwinbijoywork@gmail.com>
2025-05-27 22:57:16 +0530

    Update app.py

--- voicemorpherai-backend/app.py
+++ voicemorpherai-backend/app.py
@@ -86,6 +86,5 @@ def clone():
         print("Voice clone error:", e)
         return jsonify({"error": str(e)}), 500
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=5050, debug=True)
+port = int(os.environ.get("PORT", 10000))  # Render will set this
+app.run(host="0.0.0.0", port=port)
